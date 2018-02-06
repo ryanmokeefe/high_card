f5edd0dc191d05a8951f23b1927ab21d1cf5b93f
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-02-05 23:01:31 -0500

    not sorting, not pushing winners

--- high_card.rb
+++ high_card.rb
@@ -4,17 +4,68 @@ suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
 
-# create deck:
 card_deck = []
-# create card - add value and suit
+
+# Create deck - add value and suit:
 suits.each do |suit|
     ranks.map do |rank|
-        card = {value: rank, suit: suit}
-        card_deck.push(card)
+        card_deck.push([rank, suit])
+    end
+end
+deck = card_deck.shuffle
+# puts card_deck
+# puts deck
+
+# Get players:
+input = ""
+until input == "play"
+    n = players.length
+    puts "#{n} players so far. Enter a player name, or type 'play':"
+    input = gets.chomp
+    break if input == "play"
+    players.push({name: input})
+end 
+
+# Deal:
+i = 0
+while i < players.length do
+    players.each do |player|
+        player[:hand] = deck[i]
+        i +=1
     end
 end
 
+puts players
 
+# find A, K, Q, J, 10: (add IF st. for a tie)
+winner = []
 
+while winner.length < 1 do
+    high_score = players.detect { |player| player["hand"] == "A" }
+    winner.push(high_score)
+    break if winner.length > 0
+    high_score = players.detect { |player| player["hand"] == "K" }    
+    winner.push(high_score)
+    break if winner.length > 0
+    high_score = players.detect { |player| player["hand"] == "Q" }
+    winner.push(high_score)
+    break if winner.length > 0
+    high_score = players.detect { |player| player["hand"] == "J" }
+    winner.push(high_score)
+    break if winner.length > 0
+    high_score = players.detect { |player| player["hand"] == "10" }
+    winner.push(high_score)
+    break if winner.length > 0
+    ranking = players.sort_by { |player| player["hand"] }
+    high_score = ranking[-1]
+    winner.push(high_score)    
+end
 
-puts card_deck
+puts "Winner(s): "
+puts winner
+
+puts "Rankings: "
+score = players.sort_by { |player| player["hand"] }
+puts score
+
+##### not sorting, not pushing winners
