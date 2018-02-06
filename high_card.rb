0f5d322ff14800dc4ac6fc59c0ca829b10d6bcfb
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-02-05 20:04:07 -0500

    .

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,18 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+# create deck:
+card_deck = []
+# create card - add value and suit
+suits.each do |suit|
+    ranks.map do |rank|
+        card = {value: rank, suit: suit}
+        card_deck.push(card)
+    end
+end
+
+
+
+
+puts card_deck
