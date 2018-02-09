c0f5a7f05ce7a1e71071c9d99ca66b02497fad1c
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-02-09 09:06:05 -0500

    not complete

--- high_card.rb
+++ high_card.rb
@@ -1,9 +1,9 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-# ranks = ["A", "J"]
+# ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = ["A", 10, "J", "Q", "K"]
 
 suits = [ "hearts", "spades", "clubs", "diamonds"]
- 
+
 
 players = []
 
@@ -43,36 +43,36 @@ puts players
 # find A, K, Q, J, 10: (add IF st. for a tie)
 $winner = []
 
-# high_score = players.select { |player| player[:hand][0] == "A" }
-# $winner << high_score
+high_score = players.select { |player| player[:hand][0] == "A" || player[:hand][0] == "K" || player[:hand][0] == "Q" || player[:hand][0] == "J" || player[:hand][0] == 10  }
+$winner << high_score
 
-    high_score = players.select { |player| player[:hand][0] == "A" }
-    $winner << high_score
-    if $winner.length == 0
-        high_score = players.select { |player| player[:hand][0] == "K" }
-        $winner << high_score
-    end 
-    if $winner.length == 0
-        high_score = players.select { |player| player[:hand][0] == "Q" }
-        $winner << high_score
-    end 
-    if $winner.length == 0
-        high_score = players.select { |player| player[:hand][0] == "J" }
-        $winner << high_score
-    end 
-    if $winner.length == 0
-        high_score = players.select { |player| player[:hand][0] == 10 }
-        $winner << high_score
-    end 
-    if $winner.length == 0
-        ranking = players.sort_by! { |player| player[:hand][0] }
-        high_score = ranking[-1]
-        $winner << high_score 
-    end
+    # high_score = players.select { |player| player[:hand][0] == "A" }
+    # $winner << high_score
+    # if $winner.length == 0
+    #     high_score = players.select { |player| player[:hand][0] == "K" }
+    #     $winner << high_score
+    # end 
+    # if $winner.length == 0
+    #     high_score = players.select { |player| player[:hand][0] == "Q" }
+    #     $winner << high_score
+    # end 
+    # if $winner.length == 0
+    #     high_score = players.select { |player| player[:hand][0] == "J" }
+    #     $winner << high_score
+    # end 
+    # if $winner.length == 0
+    #     high_score = players.select { |player| player[:hand][0] == 10 }
+    #     $winner << high_score
+    # end 
+    # if $winner.length == 0
+    #     ranking = players.sort_by! { |player| player[:hand][0] }
+    #     high_score = ranking[-1]
+    #     $winner << high_score 
+    # end
        
 
 
-# ranking = players.sort_by! { |player| player[:hand] }
+ranking = players.sort_by! { |player| player[:hand] }
 
 
 
@@ -82,7 +82,7 @@ $winner = []
 
 
 puts "Ranked:"
-# puts ranking
+puts ranking
 puts "Winner(s): "
 puts $winner
 
